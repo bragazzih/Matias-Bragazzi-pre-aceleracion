dd43d21c4f69d23f3a9d236da5605f782724b83d
Matias Bragazzi <bragazzih@gmail.com>
2021-08-30 17:36:59 -0300

    No interactua con la tabla usuarios pero ya tiene login.

---
+++ Disney/src/main/java/com/example/Disney/security/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.example.Disney.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Bean;
+
+
+@Configuration
+@EnableWebSecurity
+
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Autowired
+UserDetailsService userDetailsService;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/admin").hasRole("ADMIN")
+		.antMatchers("/user").hasRole("ADMIN,USER")
+		.antMatchers("/").permitAll()
+		.and().formLogin();
+		
+	}
+	
+@Bean
+public PasswordEncoder getPasswodEncoder() {
+	return NoOpPasswordEncoder.getInstance();
+}
+
+	
+	
+
+	
+}
